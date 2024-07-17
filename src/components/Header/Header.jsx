3829f50509b93ae4368cc09917f19a5e1b7f1119
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-07-18 02:43:27 +0400

    created header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import imageLogo from "../../assets/headerImages/Foody..png";
+import "../../style/header.css";
 
 function Header() {
   return (
-    <div>
-      <h1>Hello Nav</h1>
+    <div className="navBar">
+      <div className="logoLinks">
+        <img src={imageLogo} alt="" className="imgLogo" />
+
+        <ul>
+          <li>Home</li>
+          <li>Restaurants</li>
+          <li>About us</li>
+          <li>How it works</li>
+          <li>FAQs</li>
+        </ul>
+      </div>
+
+      <div className="inputBtns">
+        <input type="text" />
+        <button className="langBtn">LANG</button>
+        <button className="inpSignUp">Sign up</button>
+      </div>
     </div>
   );
 }
