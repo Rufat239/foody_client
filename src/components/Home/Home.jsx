e8bbff2665de44bae08be5f6e7764dd5c00065e1
Qelemnaz90 <enazy1990@gmail.com>
2024-07-22 10:29:51 +0400

    fixed responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -25,7 +25,7 @@ return (
         <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
     </div>
 
-    <div>
+    <div className='regor'>
         <button className="register">Register</button>
         <button className="order">Order now</button>
     </div>
