72f06ac0f96371b095215ba5e7f0cd6293ad9573
RasulGv <garavaliyevrasul531@gmail.com>
2024-07-24 21:52:14 +0400

    created the Howwork Aboutus Faq pages and responsive design

--- src/components/Aboutus/Aboutus.jsx
+++ src/components/Aboutus/Aboutus.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import '../../style/aboutus.css'
+import hamburger from '../../assets/aboutusImages/hamburger.svg';
+import pizza from '../../assets/aboutusImages/pizza.svg';
+import soup from '../../assets/aboutusImages/soup.svg';
+import coffee from '../../assets/aboutusImages/coffee.svg';
 
-function Aboutus() {
+
+function About() {
   return (
-    <div>nkk</div>
-  )
-}
+        <div className="about">
+          <div className="about-container">
+            <div className="about-us">
+              <h1>About Us</h1>
+              <p>
+                Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
+              </p>
+            </div>
+            <div className="about-background">
+              <div className="about-menu">
+                {/* <div className="about-hamburger"> */}
+                  <img className='hamburger' src={hamburger} alt="Hamburger" />
+                  {/* </div> */}
+                </div>
+                {/* <div className="about-pizza"> */}
+                  <img className='pizza' src={pizza} alt="Sausage Pizza" />
+                {/* </div> */}
+                {/* <div className="about-soup"> */}
+                  <img className='soup' src={soup} alt="Tomato Soup" />
+                {/* </div> */}
+                {/* <div className="about-coffee"> */}
+                  <img className='coffee' src={coffee} alt="Papa Coffee" />
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+      );
+    }
+    
+  
 
-export default Aboutus
+export default About;
