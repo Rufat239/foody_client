bdf8c6593bf06c306dc96d93f0b24a50ee8c8ea0
NemanHJ <h.neman36@gmail.com>
2024-07-25 11:58:44 +0400

    Profile-page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Profile from '../components/Profile/Profile'
+import Sidebar from '../components/Sidebar/Sidebar'
+
+
+function ProfilePage() {
+  return (
+    <div style={{ display: "flex", marginTop: "2%", columnGap: "2%" }}>
+      <Sidebar />
+      <Profile />
+    </div>
+  )
+}
+
+export default ProfilePage
