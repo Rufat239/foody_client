2dbcb3d5ba013a24a68252cc9b9473555ab9ecca
Zibeydee <zibeydeceferli2012@gmail.com>
2024-07-25 20:27:48 +0400

    added logout modal to the project

---
+++ src/components/Logout/Logout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../../style/logout.css'
+import logoutImg from '../../assets/ordersImages/box-arrow-right.svg'
+
+function Logout() {
+  return (
+    <div className='logout-overlay'>
+    <div className=' logout-container'>
+        <div className='text-logoutModal'>
+            <h1>Are you sure you want to logout?</h1>
+            <p>You will need to log in again to continue.</p>
+        </div>
+        <div className='logout-buttons'>
+           <button onClick={onCancel} className='logout-cancel-button'>Cancel</button>
+           <button className='logout-button'> <img src={logoutImg} /> <p>Logout</p> </button>
+        </div>
+    </div>
+
+  </div>
+  )
+}
+
+export default Logout
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../style/orders.css'
 import options from '../../assets/ordersImages/Group 377.svg'
+import PrevButton from '../../assets/ordersImages/chevron-left.svg'
+import NextButton from '../../assets/ordersImages/chevron-right.svg'
 import DeleteModal from '../DeleteModal/DeleteModal';
 import ShowModal from '../ShowModal/ShowModal';
 
-function Orders() {
-
+function Orders({ itemsPerPageOptions = [5, 10, 15] }) {
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(itemsPerPageOptions[0])
+  const [totalPages, setTotalPages] = useState(1)
 
   const [menuVisible, setMenuVisible] = useState(null);
   const [visibleDeleteModal, setVisibleDeleteModal] = useState(false)
-  const [visibleShowModal, setVisibleShowModal] =useState(false)
+  const [visibleShowModal, setVisibleShowModal] = useState(false)
 
   const toggleMenu = (id) => {
     if (menuVisible === id) {
@@ -62,73 +66,166 @@ function Orders() {
       amount: '$249.7',
       payment: 'Cash On Delivery',
       contact: '994-51-410-3130'
+    },
+    {
+      id: '9180',
+      time: '25 Dec 2021',
+      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
+      amount: '$249.7',
+      payment: 'Cash On Delivery',
+      contact: '994-51-410-3130'
+    },
+    {
+      id: '9181',
+      time: '25 Dec 2021',
+      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
+      amount: '$249.7',
+      payment: 'Cash On Delivery',
+      contact: '994-51-410-3130'
+    },
+    {
+      id: '9182',
+      time: '25 Dec 2021',
+      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
+      amount: '$249.7',
+      payment: 'Cash On Delivery',
+      contact: '994-51-410-3130'
+    },
+    {
+      id: '9183',
+      time: '25 Dec 2021',
+      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
+      amount: '$249.7',
+      payment: 'Cash On Delivery',
+      contact: '994-51-410-3130'
+    },
+    {
+      id: '9184',
+      time: '25 Dec 2021',
+      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
+      amount: '$249.7',
+      payment: 'Cash On Delivery',
+      contact: '994-51-410-3130'
+    },
+    {
+      id: '9185',
+      time: '25 Dec 2021',
+      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
+      amount: '$249.7',
+      payment: 'Cash On Delivery',
+      contact: '994-51-410-3130'
     }
   ]
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(Orders.length / itemsPerPage))
+  }, [Orders, itemsPerPage])
+
+  const changePage = (x) => {
+    setCurrentPage((prev) => {
+      const newPage = Math.max(1, Math.min(totalPages, prev + x));
+      return newPage;
+    });
+  }
+
+  const changeItemsPerPage = (e) => {
+    setItemsPerPage(Number(e.target.value))
+    setCurrentPage(1)
+  }
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const selectedItems = Orders.slice(startIndex, startIndex + itemsPerPage);
+
 
 
 
   return (
-    <div className='orderContainer'>
-      <div className='titleOrders'>
-        <h1>Your Orders</h1>
-      </div>
 
 
-      <div className='tableContainer'>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Time</th>
-              <th>Delivery Address</th>
-              <th>Amount</th>
-              <th>Payment Method</th>
-              <th>Contact</th>
-              <th></th>
-            </tr>
-          </thead>
-          {Orders.map((x) => (
-            <tbody>
-              <tr >
-                <td className='idOrders'>
-                  <p>{x.id}</p>
-                </td>
-                <td>{x.time}</td>
-                <td className='ordersAddress'>{x.address}</td>
-                <td>{x.amount}</td>
-                <td>{x.payment}</td>
-                <td>{x.contact}</td>
-                <td onClick={toggleMenu}
-                  onMouseEnter={() => setMenuVisible(x.id)}
-                  onMouseLeave={() => setMenuVisible(null)}
-                  className='optionsButtonCell'>
-                  <img
-                    className='optionsButton'
-                    src={options} />
-                </td>
-                {menuVisible === x.id && (
-                  <div
+    <div className='all-order-component'>
+      <div className='orderContainer'>
+        <div className='titleOrders'>
+          <h1>Your Orders</h1>
+        </div>
+
+
+        <div className='tableContainer'>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Time</th>
+                <th>Delivery Address</th>
+                <th>Amount</th>
+                <th>Payment Method</th>
+                <th>Contact</th>
+                <th></th>
+              </tr>
+            </thead>
+            {selectedItems.map((x) => (
+              <tbody>
+                <tr >
+                  <td className='idOrders'>
+                    <p>{x.id}</p>
+                  </td>
+                  <td>{x.time}</td>
+                  <td className='ordersAddress'>{x.address}</td>
+                  <td>{x.amount}</td>
+                  <td>{x.payment}</td>
+                  <td>{x.contact}</td>
+                  <td onClick={toggleMenu}
                     onMouseEnter={() => setMenuVisible(x.id)}
                     onMouseLeave={() => setMenuVisible(null)}
-                    className='dropDownMenuContent'>
-                    <p onClick={handleVisibleShowModal} className='showLink'>show</p>
-                    <p onClick={handleShowDeleteModal} className='deleteLink'>delete</p>
-                  </div>
+                    className='optionsButtonCell'>
+                    <img
+                      className='optionsButton'
+                      src={options} />
+                  </td>
+                  {menuVisible === x.id && (
+                    <div
+                      onMouseEnter={() => setMenuVisible(x.id)}
+                      onMouseLeave={() => setMenuVisible(null)}
+                      className='dropDownMenuContent'>
+                      <p onClick={handleVisibleShowModal} className='showLink'>show</p>
+                      <p onClick={handleShowDeleteModal} className='deleteLink'>delete</p>
+                    </div>
+                  )}
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+
+        <div className="pagination-order">
+          <div className="prev-next-order">
+            <button onClick={() => changePage(-1)} className="prev-button-order"> <img src={PrevButton} /> </button>
+            <span className="currentpage-order">{currentPage}</span> <span className="slash-order">/</span> <span className="total-pages-order">{totalPages}</span>
+            <button onClick={() => changePage(1)} className="next-button-order"> <img src={NextButton} /> </button>
+          </div>
+          <div className="select-options-order">
+            <div className="select-container-order">
+              <select value={itemsPerPage} onChange={changeItemsPerPage} className="custom-select-order">
+                {itemsPerPageOptions.map((option) =>
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
                 )}
-              </tr>
-            </tbody>
-          ))}
-        </table>
+              </select>
+              <div className="select-arrow-order">&#9660;</div>
+            </div>
+          </div>
+        </div>
+        {visibleDeleteModal && (
+          <DeleteModal
+            onCancel={handleCancel} />
+        )}
+        {visibleShowModal && (
+          <ShowModal
+            onClose={handleClose} />
+        )}
       </div>
-      {visibleDeleteModal && (
-        <DeleteModal 
-        onCancel={handleCancel}/>
-      )}
-      {visibleShowModal && (
-        <ShowModal
-        onClose = {handleClose}/>
-      )}
     </div>
+
   )
 }
 
