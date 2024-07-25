f6f59d58f02aa7b3d279310d283d71310b35a2ae
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-07-25 20:20:17 +0400

    footer connected layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MainContent from "../MainContent/MainContent";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function Layout({ children }) {
   return (
     <div>
       <Header />
       <MainContent>{children}</MainContent>
+      <Footer />
     </div>
   );
 }
