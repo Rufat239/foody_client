eccad48e4d5e0c25001b629191263e3ebc44a948
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-07-26 14:01:06 +0400

    changed some css codes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import MainContent from "../MainContent/MainContent";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import "../../style/layout.css";
 
 function Layout({ children }) {
   return (
