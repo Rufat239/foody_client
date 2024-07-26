b67c9e243065e87cbc38e3dc09148b1235870342
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-07-26 13:35:13 +0400

    created new css pages

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import MainContent from "../MainContent/MainContent";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import "../../style/layout.css";
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className="clientLayout">
       <Header />
       <MainContent>{children}</MainContent>
       <Footer />
