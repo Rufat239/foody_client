e58f33c02b4a3e0e5d90f7e691ddae0fc9a5546f
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-07-29 03:51:15 +0400

    English, French and Azerbaijani languages ​​were written for navbar

---
+++ src/i18n.js
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en/translation.json";
+import fr from "./locales/fr/translation.json";
+import az from "./locales/az/translation.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    fr: {
+      translation: fr,
+    },
+    az: {
+      translation: az,
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
