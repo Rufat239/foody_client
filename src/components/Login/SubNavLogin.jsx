eaae62fc8117dadb2540d177958b79c451634537
Nurane2003 <nurane.sirinova.2003@gmail.com>
2024-07-28 23:32:05 +0400

    Set the login page with useState

---
+++ src/components/Login/SubNavLogin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import "../../style/header.css"
+import bayraq from '../../assets/loginimages/Rectangle.svg'
+
+function SubNavLogin() {
+  return (
+    <div className='login-container'>
+      <div className='login-title'>
+        <h1 className='login-name'>Foody.</h1>
+        <button className='login-button' ><img src={bayraq} alt="" className='login-image' /></button>
+      </div>
+    
+    </div>
+  )
+}
+
+export default SubNavLogin
