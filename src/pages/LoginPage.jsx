461bca894a827764b02a92accb3b406182ea4ee2
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-08-01 13:08:39 +0400

    commit

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,26 @@
-import React , { useState } from 'react'
-import '../style/loginPage.css'
-import SubNavLogin from '../components/Login/SubNavLogin';
-import Login from '../components/Login/Login';
-import Register from '../components/Register/Register';
+import React, { useState } from "react";
+import "../style/loginPage.css";
+import SubNavLogin from "../components/Login/SubNavLogin";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
 
 function LoginPage() {
-   const[currentForm, setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState("login");
 
-   const toggleForm = (formName) => {
-     setCurrentForm(formName);
-   }
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
   return (
     <div>
-        <SubNavLogin/>
+      <SubNavLogin />
 
-        {
-        currentForm ==="login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
-        }
-
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
