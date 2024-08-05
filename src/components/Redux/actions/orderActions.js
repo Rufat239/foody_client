e79d87a8970da6b1c3ad8737354051079c007fdf
S U R A Y  M I R Z E Y E V A <mirzayevasuray3@gmail.com>
2024-08-05 21:44:28 +0400

    zibeydenin yazdigi checkoutredux hissesni push edirem

---
+++ src/components/Redux/actions/orderActions.js
@@ -0,0 +1,5 @@
+export const ADD_ORDER = 'ADD_ORDER';
+export const addOrder = (order) => ({
+  type: ADD_ORDER,
+  payload: order,
+});
--- src/components/Redux/reducers/index.js
+++ src/components/Redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import basketReducer from '../reducers/basketReducer';
+import orderReducer from '../reducers/orderReducer';
 
 const rootReducer = combineReducers({
   basket: basketReducer,
+  order:orderReducer
 });
 
 export default rootReducer;
