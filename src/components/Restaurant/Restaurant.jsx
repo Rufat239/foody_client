dbfaeed4034d82cb718699ca2bc1a4bd2ade3787
S U R A Y  M I R Z E Y E V A <mirzayevasuray3@gmail.com>
2024-08-06 02:06:29 +0400

    cards height duzelis

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -95,25 +95,31 @@ function Restaurant() {
                                                                         {/* Restaurant Cards */}
       <section className='restaurantCards'>
         {filteredRestaurants.map((restaurant, index) => (
-          <div className='restaurants_card' key={index}>
-            <Link to={'/internal'} className='cardLink'>
-            <div className="newBadge">
-                {index === 1 && <div className='newlist'>New</div>}
-                </div>
-              <div className='restaurant_cardImage'>
-                <img src={restaurant.image} alt={restaurant.name} />
-              </div>
-              <div className='restaurants_Info'>
-                <h2>{restaurant.name}</h2>
-                <p>{restaurant.description}</p>
-              </div>
-              <div className='delivery'>
-                <p>{restaurant.price}</p>
-                <span>{restaurant.time}</span>
-              </div>
+            <div className="cardHeight">
+              <div className='restaurants_card' key={index}>
           
-            </Link>
-          </div>
+          <Link to={'/internal'} className='cardLink'>
+          <div className="newBadge">
+              {index === 1 && <div className='newlist'>New</div>}
+              </div>
+            <div className='restaurant_cardImage'>
+              <img src={restaurant.image} alt={restaurant.name} />
+            </div>
+            <div className='restaurants_Info'>
+              <h2>{restaurant.name}</h2>
+              <p>{restaurant.description}</p>
+            </div>
+            <div className='delivery'>
+              <p>{restaurant.price}</p>
+              <span>{restaurant.time}</span>
+            </div>
+        
+          </Link>
+        </div>
+              
+            </div>
+          
+          
         ))}
       </section>
     </div>
