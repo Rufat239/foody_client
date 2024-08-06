7cd4a2fe01c04b51fb835226585c6f895b0f5794
Zibeydee <zibeydeceferli2012@gmail.com>
2024-08-06 12:24:35 +0400

    added connection between order and checkout page

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,159 +1,80 @@
-import React, { useEffect, useState } from 'react'
-import '../../style/orders.css'
-import options from '../../assets/ordersImages/Group 377.svg'
-import PrevButton from '../../assets/ordersImages/chevron-left.svg'
-import NextButton from '../../assets/ordersImages/chevron-right.svg'
+import React, { useEffect, useState } from 'react';
+import '../../style/orders.css';
+import options from '../../assets/ordersImages/Group 377.svg';
+import PrevButton from '../../assets/ordersImages/chevron-left.svg';
+import NextButton from '../../assets/ordersImages/chevron-right.svg';
 import DeleteModal from '../DeleteModal/DeleteModal';
 import ShowModal from '../ShowModal/ShowModal';
+import { useSelector } from 'react-redux';
 
-function Orders({ itemsPerPageOptions = [5, 10, 15] }) {
-  const [currentPage, setCurrentPage] = useState(1)
-  const [itemsPerPage, setItemsPerPage] = useState(itemsPerPageOptions[0])
-  const [totalPages, setTotalPages] = useState(1)
+function Orders({ orders = [], itemsPerPageOptions = [5, 10, 15] }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(itemsPerPageOptions[0]);
+  const [totalPages, setTotalPages] = useState(1);
 
   const [menuVisible, setMenuVisible] = useState(null);
-  const [visibleDeleteModal, setVisibleDeleteModal] = useState(false)
-  const [visibleShowModal, setVisibleShowModal] = useState(false)
+  const [visibleDeleteModal, setVisibleDeleteModal] = useState(false);
+  const [visibleShowModal, setVisibleShowModal] = useState(false);
+
+  const orderList = useSelector((state) => state.order.orders);
+  const totalPrice = useSelector((state) => state.basket.totalPrice);
 
   const toggleMenu = (id) => {
+    console.log('menu')
     if (menuVisible === id) {
-      setMenuVisible(null)
+      setMenuVisible(null);
     } else {
-      setMenuVisible(id)
+      setMenuVisible(id);
     }
   };
 
   const handleShowDeleteModal = () => {
-    setVisibleDeleteModal(true)
+    setVisibleDeleteModal(true);
   };
 
   const handleVisibleShowModal = () => {
-    setVisibleShowModal(true)
-  }
-
-
+    setVisibleShowModal(true);
+  };
 
   const handleCancel = () => {
-    setVisibleDeleteModal(false)
-  }
+    setVisibleDeleteModal(false);
+  };
 
   const handleClose = () => {
-    setVisibleShowModal(false)
-  }
-
-
-  const Orders = [
-    {
-      id: '9177',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9178',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9179',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9180',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9181',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9182',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9183',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9184',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    },
-    {
-      id: '9185',
-      time: '25 Dec 2021',
-      address: '29 Eve Street, 543 Evenue Road, Ny 87876 ',
-      amount: '$249.7',
-      payment: 'Cash On Delivery',
-      contact: '994-51-410-3130'
-    }
-  ]
+    setVisibleShowModal(false);
+  };
 
   useEffect(() => {
-    setTotalPages(Math.ceil(Orders.length / itemsPerPage))
-  }, [Orders, itemsPerPage])
+     console.log('Order list ', orderList);
+    setTotalPages(Math.ceil(orderList.length / itemsPerPage));
+  }, [orderList, itemsPerPage]);
 
   const changePage = (x) => {
     setCurrentPage((prev) => {
       const newPage = Math.max(1, Math.min(totalPages, prev + x));
       return newPage;
     });
-  }
+  };
 
   const changeItemsPerPage = (e) => {
-    setItemsPerPage(Number(e.target.value))
-    setCurrentPage(1)
-  }
+    setItemsPerPage(Number(e.target.value));
+    setCurrentPage(1);
+  };
 
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const selectedItems = Orders.slice(startIndex, startIndex + itemsPerPage);
-
-
-
+  const selectedItems = orderList.slice(startIndex, startIndex + itemsPerPage);
 
   return (
-
-
     <div className='all-order-component'>
       <div className='order-container'>
         <div className='title-orders'>
           <h1>Your Orders</h1>
         </div>
-
-
         <div className='table-container'>
           <table>
             <thead>
               <tr>
-              <th className='for-small-screen'></th>
+                <th className='for-small-screen'></th>
                 <th>ID</th>
                 <th>Time</th>
                 <th>Delivery Address</th>
@@ -163,82 +84,68 @@ function Orders({ itemsPerPageOptions = [5, 10, 15] }) {
                 <th className='options-button-cell'></th>
               </tr>
             </thead>
-            {selectedItems.map((x) => (
-              <tbody>
-                <tr >
-                <td onClick={() =>toggleMenu(x.id)}
-                    className='for-small-screen'>
-                    <img
-                      className='options-button'
-                      src={options} />
-                         {menuVisible === x.id && (
-                    <div
-                      className='drop-down-menu-content'>
-                      <p onClick={handleVisibleShowModal} className='show-setting'>show</p>
-                      <p onClick={handleShowDeleteModal} className='delete-setting'>delete</p>
-                    </div>
-                  )}
+            <tbody>
+              {selectedItems.map((item) => (
+                <tr key={item.id}>
+                  <td onClick={() => toggleMenu(item.id)} className='for-small-screen'>
+                    <img className='options-button' src={options} alt="options" />
+                    {menuVisible === item.id && (
+                      <div className='drop-down-menu-content'>
+                        <p onClick={handleVisibleShowModal} className='show-setting'>show</p>
+                        <p onClick={handleShowDeleteModal} className='delete-setting'>delete</p>
+                      </div>
+                    )}
                   </td>
-               
                   <td className='id-orders'>
-                    <p>{x.id}</p>
+                    <p>{item.id.toString().slice(0, 4)}</p>
                   </td>
-                  <td>{x.time}</td>
-                  <td className='orders-address'>{x.address}</td>
-                  <td>{x.amount}</td>
-                  <td>{x.payment}</td>
-                  <td>{x.contact}</td>
-                  <td onClick={() =>toggleMenu(x.id)}
-                    className='options-button-cell'>
-                    <img
-                      className='options-button'
-                      src={options} />
-                         {menuVisible === x.id && (
-                    <div
-                      className='drop-down-menu-content'>
-                      <p onClick={handleVisibleShowModal} className='show-setting'>show</p>
-                      <p onClick={handleShowDeleteModal} className='delete-setting'>delete</p>
-                    </div>
-                  )}
+                  <td>{item.time}</td>
+                  <td className='orders-address'>{item.deliveryAddress}</td>
+                  <td>${item.totalPrice.toFixed(2)}</td>
+                  <td>{item.paymentMethod}</td>
+                  <td>{item.contactNumber}</td>
+                  <td onClick={() => toggleMenu(item.id)} className='options-button-cell'>
+                    <img className='options-button' src={options} alt="options" />
+                    {menuVisible === item.id && (
+                      <div className='drop-down-menu-content'>
+                        <p onClick={handleVisibleShowModal} className='show-setting'>show</p>
+                        <p onClick={handleShowDeleteModal} className='delete-setting'>delete</p>
+                      </div>
+                    )}
                   </td>
-               
                 </tr>
-              </tbody>
-            ))}
+              ))}
+            </tbody>
           </table>
         </div>
-
         <div className="pagination-order">
           <div className="prev-next-order">
-            <button onClick={() => changePage(-1)} className="prev-button-order"> <img src={PrevButton} /> </button>
+            <button onClick={() => changePage(-1)} className="prev-button-order" disabled={currentPage === 1}>
+              <img src={PrevButton} alt="previous" />
+            </button>
             <span className="currentpage-order">{currentPage}</span> <span className="slash-order">/</span> <span className="total-pages-order">{totalPages}</span>
-            <button onClick={() => changePage(1)} className="next-button-order"> <img src={NextButton} /> </button>
+            <button onClick={() => changePage(1)} className="next-button-order" disabled={currentPage === totalPages}>
+              <img src={NextButton} alt="next" />
+            </button>
           </div>
           <div className="select-options-order">
             <div className="select-container-order">
               <select value={itemsPerPage} onChange={changeItemsPerPage} className="custom-select-order">
-                {itemsPerPageOptions.map((option) =>
+                {itemsPerPageOptions.map((option) => (
                   <option key={option} value={option}>
                     {option}
                   </option>
-                )}
+                ))}
               </select>
               <div className="select-arrow-order">&#9660;</div>
             </div>
           </div>
         </div>
-        {visibleDeleteModal && (
-          <DeleteModal
-            onCancel={handleCancel} />
-        )}
-        {visibleShowModal && (
-          <ShowModal
-            onClose={handleClose} />
-        )}
+        {visibleDeleteModal && <DeleteModal onCancel={handleCancel} />}
+        {visibleShowModal && <ShowModal onClose={handleClose} />}
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Orders
+export default Orders;
