722bfe4507579ae4b2790041fcdf901a2a826d7b
Zibeydee <zibeydeceferli2012@gmail.com>
2024-10-01 00:14:29 +0400

    changed postCheckoutDatas code

--- src/service/checkout/postCheckout.js
+++ src/service/checkout/postCheckout.js
@@ -1,15 +1,10 @@
 import axios from "axios";
-
 async function postCheckoutDatas(order) {
-    const checkoutUrl = `https://test-foody-admin-default-rtdb.firebaseio.com/orders.json`
-
-
+    const checkoutUrl = `https://test-foody-admin-default-rtdb.firebaseio.com/orders/${order.id}.json`
     try {
-        const response = await axios.post(checkoutUrl, order)
+        const response = await axios.patch(checkoutUrl, order)
+        
     } catch (error) {
-
     }
-
 }
-
 export default postCheckoutDatas
