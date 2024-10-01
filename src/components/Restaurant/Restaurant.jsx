3a903abb0db61af652fe93a52b77c74c4981c735
Zibeydee <zibeydeceferli2012@gmail.com>
2024-10-02 02:56:20 +0400

    added getCategory restaurant, filtering by categories and fixed some css codes

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -1,59 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import '../../style/restaurant.css';
-import pizza from '../../assets/restaurant_images/Pizza.jpg';
-import burger from '../../assets/restaurant_images/burger.jpg';
-import mania from '../../assets/restaurant_images/mania.jpg';
 import filterImg from '../../assets/restaurant_images/filter_list.jpg';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const restaurants = [
-  { image: pizza, name: 'Anadolu Restoran', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: burger, name: 'Burger King', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: pizza, name: 'Anadolu Restoran', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: mania, name: 'Coffee Mania', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: mania, name: 'Coffee Mania', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: burger, name: 'Burger King', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: pizza, name: 'Anadolu Restoran', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' },
-  { image: mania, name: 'Coffee Mania', description: 'chinese, sea-food, thai, lebanese, caribbean', price: '5$ delivery', time: '9 min' }
-];
-
-const filter = [
-  { image: pizza, menu: 'All' },
-  { image: pizza, menu: 'Azerbaijan' },
-  { image: pizza, menu: 'Korean' },
-  { image: pizza, menu: 'Chinese' },
-  { image: pizza, menu: 'Pizza' },
-  { image: pizza, menu: 'Indian' },
-  { image: pizza, menu: 'Kabab' },
-  { image: pizza, menu: 'Fast Food' },
-  { image: pizza, menu: 'Sea Food' },
-  { image: pizza, menu: 'Indian' },
-  { image: pizza, menu: 'Fast Food' },
-  { image: pizza, menu: 'Sea Food' },
-  { image: pizza, menu: 'Indian' },
-  { image: pizza, menu: 'Kabab' },
-  { image: pizza, menu: 'Fast Food' },
-  { image: pizza, menu: 'Sea Food' },
-];
-
 function Restaurant() {
   const [selectedMenu, setSelectedMenu] = useState('All');
   const [isFilterMenuVisible, setIsFilterMenuVisible] = useState(false);
-
   const [restaurantsData, setRestaurantsData] = useState([]);
-
+  const [category, setCategory] = useState([]);
 
   //  GET RESTAURANT DATAS
-
   useEffect(() => {
     const getRestaurants = async () => {
       const restaurantURL = `https://test-foody-admin-default-rtdb.firebaseio.com/restaurants.json`;
       try {
         const response = await axios.get(restaurantURL);
         const data = response.data;
-        console.log(data, "data");
-
         setRestaurantsData(Object.values(data));
       } catch (error) {
         console.log("error", error);
@@ -61,6 +24,22 @@ function Restaurant() {
     };
 
     getRestaurants();
+  }, []);
+
+  // GET CATEGORY DATAS
+  useEffect(() => {
+    const getCategoryDatas = async () => {
+      const categorUrl = `https://test-foody-admin-default-rtdb.firebaseio.com/categories.json`;
+
+      try {
+        const response = await axios.get(categorUrl);
+        const dataCategory = response.data;
+        setCategory(Object.values(dataCategory));
+      } catch (error) {
+        console.log("error");
+      }
+    };
+    getCategoryDatas();
   }, []);
 
   const handleMenuClick = (menu) => {
@@ -72,21 +51,22 @@ function Restaurant() {
     setIsFilterMenuVisible(!isFilterMenuVisible);
   };
 
+ 
   const filteredRestaurants = selectedMenu === 'All'
     ? restaurantsData
-    : restaurantsData.filter(restaurant => restaurant.description.toLowerCase().includes(selectedMenu.toLowerCase()));
+    : restaurantsData.filter(restaurant => restaurant.category.toLowerCase() === selectedMenu.toLowerCase());
 
   return (
     <div className='main-restaurant'>
       {/* Sidebar */}
-      <section className={`restaurant_sidebar `}>
+      <section className={`restaurant_sidebar`}>
         <div className='restaurant_body'>
-          {filter.map((item, index) => (
-            <div key={index} className='restaurant_side' onClick={() => handleMenuClick(item.menu)}>
+          {category.map((item, index) => (
+            <div key={index} className='restaurant_side' onClick={() => handleMenuClick(item.name)}>
               <div className='image'>
-                <img src={item.image} alt={item.menu} />
+                <img src={item.url} alt={item.name} />
               </div>
-              <span>{item.menu}</span>
+              <span>{item.name}</span>
             </div>
           ))}
         </div>
@@ -102,12 +82,12 @@ function Restaurant() {
         </div>
 
         {isFilterMenuVisible && (
-          <div className="filterMenu">
+          <div className='filterMenu'> 
             <button className='filterDelete' onClick={toggleFilterMenu}>x</button>
             <ul className='menuList'>
-              {filter.map((item, index) => (
-                <li key={index} onClick={() => handleMenuClick(item.menu)}>
-                  <p>{item.menu}</p>
+              {category.map((item, index) => (
+                <li key={index} onClick={() => handleMenuClick(item.name)}>
+                  <p>{item.name}</p>
                 </li>
               ))}
             </ul>
@@ -117,13 +97,13 @@ function Restaurant() {
 
       {/* Restaurant Cards */}
       <section className="restaurantCards">
-        {restaurantsData.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           <div className="cardHeight" key={restaurant.id}>
             <div className="restaurants_card" style={{ position: "relative" }}>
               {restaurant.isNew && <div className="newlist">New</div>}
 
               {/* INTERNAL LINK */}
-              <Link to="/internal" state={{ restaurant }}>   
+              <Link to="/internal" state={{ restaurant }}>
                 <div className="restaurant_cardImage">
                   <img src={restaurant.url} alt={restaurant.name} />
                 </div>
@@ -136,7 +116,6 @@ function Restaurant() {
                   <span>{restaurant.deliveryMin} Min</span>
                 </div>
               </Link>
-
             </div>
           </div>
         ))}
